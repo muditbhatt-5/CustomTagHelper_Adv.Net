2594a24d7ceb61d93c9993edd8b0ca53b97f6636
themudit-18 <bhattmudit213@gmail.com>
2025-02-18 09:25:21 +0530

    Add project files.

---
+++ Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace CustomTagHelper_Mark_1.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
---
+++ TagHelpers/MyCustomTagHelper.cs
@@ -0,0 +1,18 @@
+﻿using Microsoft.AspNetCore.Razor.TagHelpers;
+
+namespace CustomTagHelper_Mark_1.TagHelpers
+{
+    [HtmlTargetElement("mytag")]
+    public class MyCustomTagHelper : TagHelper
+    {
+        public string path { get; set; }
+
+        public override void Process(TagHelperContext context, TagHelperOutput output)
+        {
+            output.TagName = "button";
+            output.TagMode = TagMode.StartTagAndEndTag;
+            output.Attributes.SetAttribute("href", path);
+            output.Content.SetContent("Test");
+        }
+    }
+}
